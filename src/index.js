f0582fed7e3171e7f5cbd1c0ead81b1400fd72a7
jqc5610 <jqc5610@gmail.com>
2020-03-27 10:58:06 -0400

    final commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const beerUrl = 'http://localhost:3000/beers'
+const individualBeerUrl = ${beerUrl}/
+// set individual beer Url
+// fetch individual beer Url
+// render beer detail from individual beer Url
 let beerGroup = document.getElementsByClassName('list-group')
-let eachBeer = document.getElementsByClassName('list-group-item')
+const eachBeer = document.getElementsByClassName('list-group-item')
 
 
 document.addEventListener('DOMContentLoaded', function(event){
@@ -12,6 +16,11 @@ function getBeers(){
 }
 
 
+function getindividualBeer(){
+    return fetch(http://localhost:3000/beers/:id
+        )
+}
+
 function renderBeers(beer){
     
     let li = document.createElement('li')
@@ -19,17 +28,7 @@ function renderBeers(beer){
     li.innerText = beer.name
     console.log(beerGroup)
     beerGroup[0].appendChild(li)
-}
 
-
-getBeers()
-    .then(json => {
-        json.forEach(beer => {
-            renderBeers(beer)
-        })
-    })
-
-function renderBeerDetail(beer){
     let div = document.createElement('div')
     div.setAttribute('id', 'beer-detail')
 
@@ -42,17 +41,55 @@ function renderBeerDetail(beer){
     let p = document.createElement('p')
     p.innerText = beer.description
 
-    div.appendChild(h1, img, p)
+    div.appendChild(h1)
+    div.appendChild(img)
+    div.appendChild(p)
 
-    beerGroup[0].append(div)
+    eachBeer[0].append(div)
 }
+
 
 getBeers()
     .then(json => {
         json.forEach(beer => {
-            renderBeerDetail(beer)
+            renderBeers(beer)
         })
     })
+
+// function renderBeerDetail(beer){
+//     let div = document.createElement('div')
+//     div.setAttribute('id', 'beer-detail')
+
+//     let h1 = document.createElement('h1')
+//     h1.innerText = beer.name
+
+//     let img = document.createElement('img')
+//     img.src = beer.image_url
+
+//     let p = document.createElement('p')
+//     p.innerText = beer.description
+
+//     div.appendChild(h1)
+//     div.appendChild(img)
+//     div.appendChild(p)
+
+//     eachBeer[0].append(div)
+// }
+
+// getBeers()
+//     .then(json => {
+//         json.forEach(beer => {
+//             renderBeerDetail(beer)
+//         })
+//     })
+
+document.addEventListener('click', function(event){
+    if(event.target.className === 'list-group-item'){
+    eachBeer.style.display = 'block'
+    } else {
+        eachBeer.style.display = 'none'
+    }
+})
 
 
 
