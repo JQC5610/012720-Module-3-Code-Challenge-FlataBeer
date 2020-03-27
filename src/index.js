8e95d01c35f44a652823a6524f2239acbb567f85
jqc5610 <jqc5610@gmail.com>
2020-03-27 12:29:40 -0400

    review w/ Steven

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const beerUrl = 'http://localhost:3000/beers'
-const individualBeerUrl = ${beerUrl}/
+const individualBeerUrl = `${beerUrl}/id`
 // set individual beer Url
 // fetch individual beer Url
 // render beer detail from individual beer Url
@@ -16,45 +16,74 @@ function getBeers(){
 }
 
 
-function getindividualBeer(){
-    return fetch(http://localhost:3000/beers/:id
-        )
-}
-
-function renderBeers(beer){
-    
-    let li = document.createElement('li')
-    li.setAttribute('class', 'list-group-item')
-    li.innerText = beer.name
-    console.log(beerGroup)
-    beerGroup[0].appendChild(li)
-
-    let div = document.createElement('div')
-    div.setAttribute('id', 'beer-detail')
-
-    let h1 = document.createElement('h1')
-    h1.innerText = beer.name
-
-    let img = document.createElement('img')
-    img.src = beer.image_url
-
-    let p = document.createElement('p')
-    p.innerText = beer.description
-
-    div.appendChild(h1)
-    div.appendChild(img)
-    div.appendChild(p)
-
-    eachBeer[0].append(div)
-}
-
-
 getBeers()
     .then(json => {
         json.forEach(beer => {
             renderBeers(beer)
         })
     })
+
+
+
+function renderBeers(beer){
+
+    let li = document.createElement('li')
+    li.setAttribute('class', 'list-group-item')
+    li.innerText = beer.name
+    beerGroup[0].appendChild(li)
+    
+}
+
+
+function showBeers(beer){
+    return fetch(`individualBeerUrl/${id}`)
+    .then(resp => resp.json)
+}
+
+function showBeerDetails(beer){
+    let div = document.createElement('div')
+    div.setAttribute('id', 'beer-detail')
+
+    div.innerHTML = `
+    <h1>${beer.name}</h1>
+    <img src="${beer.image_url}">
+    <h3>${beer.tagline}</h3>
+    <p>${beer.description}</p>`
+}
+
+showBeers(beer)
+    .then()
+
+
+
+
+
+// function getindividualBeer(){
+//     return fetch(individualBeerUrl)
+//     .then(resp => resp.json)
+        
+// }
+
+
+// let div = document.createElement('div')
+//     div.setAttribute('id', 'beer-detail')
+
+//     let h1 = document.createElement('h1')
+//     h1.innerText = beer.name
+
+//     let img = document.createElement('img')
+//     img.src = beer.image_url
+
+//     let p = document.createElement('p')
+//     p.innerText = beer.description
+
+//     div.appendChild(h1)
+//     div.appendChild(img)
+//     div.appendChild(p)
+
+//     eachBeer[0].append(div)
+
+
 
 // function renderBeerDetail(beer){
 //     let div = document.createElement('div')
@@ -83,13 +112,13 @@ getBeers()
 //         })
 //     })
 
-document.addEventListener('click', function(event){
-    if(event.target.className === 'list-group-item'){
-    eachBeer.style.display = 'block'
-    } else {
-        eachBeer.style.display = 'none'
-    }
-})
+// document.addEventListener('click', function(event){
+//     if(event.target.className === 'list-group-item'){
+//     eachBeer.style.display = 'block'
+//     } else {
+//     eachBeer.style.display = 'none'
+//     }
+// })
 
 
 
