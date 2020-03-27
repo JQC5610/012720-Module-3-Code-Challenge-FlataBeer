e15b00b15396199efafe68f6651b7f282b8df06e
jqc5610 <jqc5610@gmail.com>
2020-03-27 10:35:05 -0400

    second commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const beerUrl = 'http://localhost:3000/beers'
 let beerGroup = document.getElementsByClassName('list-group')
+let eachBeer = document.getElementsByClassName('list-group-item')
 
 
 document.addEventListener('DOMContentLoaded', function(event){
-
 
 
 function getBeers(){
@@ -17,7 +17,8 @@ function renderBeers(beer){
     let li = document.createElement('li')
     li.setAttribute('class', 'list-group-item')
     li.innerText = beer.name
-    beerGroup.appendChild(li)
+    console.log(beerGroup)
+    beerGroup[0].appendChild(li)
 }
 
 
@@ -27,4 +28,32 @@ getBeers()
             renderBeers(beer)
         })
     })
+
+function renderBeerDetail(beer){
+    let div = document.createElement('div')
+    div.setAttribute('id', 'beer-detail')
+
+    let h1 = document.createElement('h1')
+    h1.innerText = beer.name
+
+    let img = document.createElement('img')
+    img.src = beer.image_url
+
+    let p = document.createElement('p')
+    p.innerText = beer.description
+
+    div.appendChild(h1, img, p)
+
+    beerGroup[0].append(div)
+}
+
+getBeers()
+    .then(json => {
+        json.forEach(beer => {
+            renderBeerDetail(beer)
+        })
+    })
+
+
+
 })
